1a0157ea39e50c83b880b863e916c218d2f4061a
SCor87 <s.cormican7@icloud.com>
2025-04-21 22:55:35 +0100

    Changes Committed Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Changes to be committed:
    new file:   8kyu/Calculate-Average.cs
    new file:   8kyu/Well-Of-Ideas-Easy Version.cs
    
    Changes not staged for commit:
    modified:   .DS_Store
    deleted:    8kyu/Calculate-Average.cs
    deleted:    8kyu/Refactor-Variable-Names-To-Variable-Keywords.cs
    deleted:    8kyu/Well-Of-Ideas-Easy Version.cs
    modified:   README.md
    
    Untracked files:
    7 kyu/
    8 kyu/

---
+++ 8kyu/Calculate-Average.cs
@@ -0,0 +1,24 @@
+/*
+    DESCRIPTION:
+
+    Write a function which calculates the average of the numbers in a given array.
+    Note: Empty arrays should return 0.
+*/
+//My Solution
+
+class AverageSolution
+{
+    public static double FindAverage(double[] array)
+    {
+        double sum = 0, average = 0, length = array.Length;
+      
+        foreach(double num in array)
+        {
+          sum += num;
+        }
+      
+        average = sum / length;
+      
+        return average;
+    }
+} 
---
+++ 8kyu/Well-Of-Ideas-Easy Version.cs
@@ -0,0 +1,35 @@
+/*
+    DESCRIPTION:
+
+    For every good kata idea there seem to be quite a few bad ones!
+
+    In this kata you need to check the provided array (x) for good ideas 'good' and 
+    bad ideas 'bad'.   
+    If there are one or two good ideas, return 'Publish!', if there are more than 
+    2 return 'I smell a series!'. If there are no good ideas, as is often the 
+    case, return 'Fail!'.
+*/
+
+//My Solution
+
+using System.Linq;
+public class Kata
+{
+  public static string Well(string[] x)
+  {
+     int wordCount = x.Count(i => i== "good"); //counting arr x for str "good" assigning it to int type variable
+    
+    if (wordCount == 0) //beginning of if, else-if statement to check for value of wordCount variable
+      {
+      return "Fail!";
+    }
+    else if(wordCount <= 2)
+      {
+      return "Publish!";
+    }
+    else
+      {
+      return "I smell a series!";
+    }
+  }
+}
