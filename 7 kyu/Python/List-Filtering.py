d0de22249e6a6976b880fe0a9e458e2630675d80
Stephen <s.cormican7@icloud.com>
2025-08-04 11:18:53 +0100

    List Filtering Python kata completed

--- 7 kyu/Python/List-Filtering.py
+++ 7 kyu/Python/List-Filtering.py
@@ -1,21 +1,8 @@
 # In this kata you will create a function that takes a list of non-negative integers
 # and strings and returns a new list with the strings filtered out.
 
-l = [1, 2, 'a', 'b']
+# l = [1, 2, 'a', 'b'] Example input from test
 
-# def filter_list(l):
-
-only_words = filter(str.isalpha, l)
-print(only_words)
-
-
-# l.remove()
-# print(l)
-
-# my_list = ['foo','bar','baz','>=','5.2']
-
-# # With
-# only_words = [token for token in my_list if token.isalpha()]
-
-# # Without
-# only_words = filter(str.isalpha, my_list)
+def filter_list(l):
+    filtered = list(filter(lambda x: isinstance(x, int), l))
+    return filtered
